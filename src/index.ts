385badf85bec7c13c3e2a3bfc5d5de1d8beec009
sodtiruk <sodtiruk22@gmail.com>
2025-02-14 22:18:15 +0700

    build: add typescript node

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
+import { Server } from "socket.io";
 
+const io = new Server(3000, {
+  cors: {
+    origin: "*",
+  },
+});
 
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("message", (msg: string) => {
+    console.log("Received:", msg);
+    socket.broadcast.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
